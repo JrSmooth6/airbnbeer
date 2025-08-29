c0b896ea89421419f1af07da495ccd757e9be5f5
JrSmooth6 <80690987+JrSmooth6@users.noreply.github.com>
2025-08-29 14:57:34 +0200

    test routing

--- src/app/core/components/landing/landing.component.ts
+++ src/app/core/components/landing/landing.component.ts
@@ -7,12 +7,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-  constructor(private router : Router){
+  constructor(private router: Router) {
 
   }
 
-  goToAdmin(){
-    this.router.navigateByUrl('/admin');
+  goToAdmin() {
+    this.router.navigate(['/admin']);          // recommandé
   }
 
 }
